c33804e4a502bc443b380f3c485daf290df53135
James O'Beirne <james.obeirne@gmail.com>
2015-11-15 23:17:15 -0800

    Enable smartfees.py RPC tests to run on travis

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -90,6 +90,7 @@ testScripts = [
     'p2p-fullblocktest.py',
     'blockchain.py',
     'disablewallet.py',
+    'smartfees.py',
 ]
 testScriptsExt = [
     'bip65-cltv.py',
@@ -107,7 +108,6 @@ testScriptsExt = [
     'receivedby.py',
 #    'rpcbind_test.py', #temporary, bug in libevent, see #6655
 #    'script_test.py', #used for manual comparison of 2 binaries
-    'smartfees.py',
     'maxblocksinflight.py',
     'invalidblockrequest.py',
     'p2p-acceptblock.py',
