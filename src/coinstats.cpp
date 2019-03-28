7f39dfd011c6921b396c5acfc8917f748c1e97dd
James O'Beirne <james.obeirne@gmail.com>
2019-03-28 12:09:06 -0400

    coinstats: add coins_count

--- src/coinstats.cpp
+++ src/coinstats.cpp
@@ -47,6 +47,7 @@ bool GetUTXOStats(CCoinsView *view, CCoinsStats &stats)
     }
     ss << stats.hashBlock;
     uint256 prevkey;
+    unsigned int coins_count{0};
     std::map<uint32_t, Coin> outputs;
     while (pcursor->Valid()) {
         boost::this_thread::interruption_point();
@@ -59,6 +60,7 @@ bool GetUTXOStats(CCoinsView *view, CCoinsStats &stats)
             }
             prevkey = key.hash;
             outputs[key.n] = std::move(coin);
+            coins_count += 1;
         } else {
             return error("%s: unable to read value", __func__);
         }
@@ -69,5 +71,6 @@ bool GetUTXOStats(CCoinsView *view, CCoinsStats &stats)
     }
     stats.hashSerialized = ss.GetHash();
     stats.nDiskSize = view->EstimateSize();
+    stats.coins_count = coins_count;
     return true;
 }
